22d1d346c7a5a4801922e94e0882217ed34717b1
Greg Delgado <grd888@gmail.com>
2026-05-13 22:33:08 +0800

    Initial Commit

---
+++ Avalanche/AvalancheApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvalancheApp.swift
+//  Avalanche
+//
+//  Created by Greg Delgado on 13/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct AvalancheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
